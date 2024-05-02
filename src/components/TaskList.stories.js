12f962795dad671f336377ab8961fb9ae5f13cff
svefur <124247388+svefur@users.noreply.github.com>
2024-05-02 14:31:37 +0200

    task lists

---
+++ src/components/TaskList.stories.js
@@ -0,0 +1,57 @@
+import TaskList from './TaskList.vue'
+import * as TaskStories from './Task.stories'
+
+export default {
+    component: TaskList,
+    title: 'TaskList',
+    tags: ['autodocs'],
+    decorators: [() => ( { template: '<div style="margin: 3em"><story/><div>' }) ],
+    ArgTypes: {
+        onPinTask: {},
+        onArchiveTask: {},
+    }
+}
+
+export const Default = {
+    args: {
+        tasks: [
+            { ...TaskStories.Default.args.task, id: '1', title: 'Tasks 1' },
+            { ...TaskStories.Default.args.task, id: '2', title: 'Tasks 2' },
+            { ...TaskStories.Default.args.task, id: '3', title: 'Tasks 3' },
+            { ...TaskStories.Default.args.task, id: '4', title: 'Tasks 4' },
+            { ...TaskStories.Default.args.task, id: '5', title: 'Tasks 5' },
+            { ...TaskStories.Default.args.task, id: '6', title: 'Tasks 6' }
+        ]
+    }
+}
+
+export const WithPinnedTasks = {
+    args: {
+        tasks: [
+            ...Default.args.tasks.slice(0, 5 ),
+            { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' }
+        ]
+    }
+}
+export const WithArchivedTasks = {
+    args: {
+        tasks: [
+            ...Default.args.tasks.slice(0, 5 ),
+            { id: '6', title: 'Task 6 (archived)', state: 'TASK_ARCHIVED' }
+        ]
+    }
+}
+
+export const Loading = {
+    args: {
+        tasks: [],
+        loading: true
+    }
+}
+
+export const Empty = {
+    args: {
+        ...Loading.args,
+        loading: false
+    }
+}
