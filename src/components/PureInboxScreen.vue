1554803680506e45c7bb764784dcbb4f55e80e13
svefur <124247388+svefur@users.noreply.github.com>
2024-05-03 16:16:10 +0200

    test

--- src/components/PureInboxScreen.vue
+++ src/components/PureInboxScreen.vue
@@ -4,7 +4,7 @@
 			<div class="wrapper-message">
 				<span class="icon-face-sad" />
 				<p class="title-message">Oh no!</p>
-				<p class="subtitle-message">Something went wrong</p>
+				<p class="subtitle-message">Something went wrongg</p>
 			</div>
 		</div>
 		<div v-else class="page lists-show">
